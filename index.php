67992a31cc692dc50818125c6b70174e3624d837
Akash Vekariya <akash.vekariya540@gmail.com>
2022-12-08 16:18:51 +0530

    Add settings json

--- index.php
+++ index.php
@@ -13,33 +13,39 @@
 </head>
 
 <body>
-	<section class="slider-wrapper">
-		<button class="slide-arrow" id="slide-arrow-prev">
-			&#8249;
-		</button>
-		<button class="slide-arrow" id="slide-arrow-next">
-			&#8250;
-		</button>
-		<ul class="slides-container" id="slides-container">
-			<li class="slide">
-				<div class="slide-section-left">
-					<a href="#">
-						<div class="blog-image-holder" style="background-image: url('./dummy.jpg')"></div>
-					</a>
-				</div>
-				<div class="post-content slide-section-right">
-					<p id="date">December 2, 2020</p>
-					<h2 id="title">Slides</h2>
-					<p id="author">Akash Vekariya</p>
-					<h5 id="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum
-						dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur
-						adipisicing elit. Quisquam, quod.
-					</h5>
-					<a href="#" id="read-more">Read More</a>
-				</div>
-			</li>
-		</ul>
-	</section>
+	<div class="gtbps container-wrapper">
+		<div class="inputWrapper">
+			<input type="text" name="dataFetchApiUrl" class="apiUrlInput" placeholder="Enter url to fetch the data from">
+			<button id="fetchData">Submit</button>
+		</div>
+		<section class="slider-wrapper">
+			<button class="slide-arrow" id="slide-arrow-prev">
+				&#8249;
+			</button>
+			<button class="slide-arrow" id="slide-arrow-next">
+				&#8250;
+			</button>
+			<ul class="slides-container" id="slides-container">
+				<li class="slide">
+					<div class="slide-section-left">
+						<a href="#">
+							<div class="blog-image-holder" style="background-image: url('./dummy.jpg')"></div>
+						</a>
+					</div>
+					<div class="post-content slide-section-right">
+						<p id="date">December 2, 2020</p>
+						<h2 id="title">Slides</h2>
+						<p id="author">Akash Vekariya</p>
+						<h5 id="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum
+							dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur
+							adipisicing elit. Quisquam, quod.
+						</h5>
+						<a href="#" id="read-more">Read More</a>
+					</div>
+				</li>
+			</ul>
+		</section>
+	</div>
 
 	<script src="./script.js"></script>
 </body>
